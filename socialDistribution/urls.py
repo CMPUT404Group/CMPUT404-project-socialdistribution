69991c41a6deb9175e3a0a735c34917973f7833e
genevag <geneva@ualberta.ca>
2016-02-19 22:23:53 -0700

    login screen - login with django, not with HTTP Basic Auth

--- socialDistribution/urls.py
+++ socialDistribution/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/profile/$', include('post.urls')),
     url(r'^api/', include('api.urls')),
     url(r'', include('post.urls')),
 )
