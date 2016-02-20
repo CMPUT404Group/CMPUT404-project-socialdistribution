75fd6e236161cbc77b1eedbf0bbbb7f190b90ea3
genevag <geneva@ualberta.ca>
2016-02-19 23:31:15 -0700

    if not logged in, redirect to login page for public & my stream

--- post/views.py
+++ post/views.py
@@ -2,28 +2,37 @@ from django.shortcuts import render
 from django.utils import timezone
 from api.models import Post
 from .forms import PostForm
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 def public_stream(request):
-	posts = Post.objects.filter(privilege='PB').order_by('date')
+	if (request.user.is_authenticated()):
 
-	form = PostForm()
-	
-	# if request.method == "POST":
-	# 	form = PostForm(request.POST)	
-	# 	if form.is_valid():
-	# 		post = form.save(commit=False)
-	# 		post.author = request.user
-	# 		# post.author = User.objects.get(username=request.user.username)
-	# 		post.publish_date = timezone.now()
-	# 		post.save()
-	# else:
-	# 	form = PostForm()
+		posts = Post.objects.filter(privilege='PB').order_by('date')
 
-	return render(request, 'post/mainStream.html', {'posts': posts, 'form':form })
+		form = PostForm()
+		
+		# if request.method == "POST":
+		# 	form = PostForm(request.POST)	
+		# 	if form.is_valid():
+		# 		post = form.save(commit=False)
+		# 		post.author = request.user
+		# 		# post.author = User.objects.get(username=request.user.username)
+		# 		post.publish_date = timezone.now()
+		# 		post.save()
+		# else:
+		# 	form = PostForm()
+
+		return render(request, 'post/mainStream.html', {'posts': posts, 'form':form })
+
+	else:
+		return HttpResponseRedirect('accounts/login')
 
 def my_stream(request):
-	posts = Post.objects.filter(author=request.user).order_by('date')
-	form = PostForm()
+	if (request.user.is_authenticated()):
+		posts = Post.objects.filter(author=request.user).order_by('date')
+		form = PostForm()
 
-	return render(request, 'post/mainStream.html', {'posts': posts, 'form':form })
+		return render(request, 'post/mainStream.html', {'posts': posts, 'form':form })
+
+	else:
+		return HttpResponseRedirect('/../accounts/login')
