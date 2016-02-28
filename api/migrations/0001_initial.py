58c0683306eb95e51aebde2a9b4077daaa9a7ebb
genevag <geneva@ualberta.ca>
2016-02-27 21:26:21 -0700

    sqllite3 settings

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -14,15 +14,30 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('comment', models.TextField()),
+                ('contentType', models.CharField(default=b'text/plain', max_length=15, choices=[(b'text/x-markdown', b'Markdown'), (b'text/plain', b'Plaintext')])),
+                ('published', models.DateTimeField(default=django.utils.timezone.now)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Post',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
+                ('contentType', models.CharField(default=b'text/plain', max_length=15, choices=[(b'text/x-markdown', b'Markdown'), (b'text/plain', b'Plaintext')])),
                 ('content', models.TextField()),
-                ('publish_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('content_type', models.CharField(default=b'PT', max_length=2, choices=[(b'MD', b'Markdown'), (b'PT', b'Plaintext')])),
-                ('privilege', models.CharField(default=b'FR', max_length=2, choices=[(b'ME', b'Me'), (b'FR', b'Friends'), (b'FF', b'Friends of Friends'), (b'PB', b'Public')])),
+                ('published', models.DateTimeField(default=django.utils.timezone.now)),
+                ('visibility', models.CharField(default=b'FRIENDS', max_length=18, choices=[(b'PUBLIC', b'Public'), (b'FOAF', b'Friends of Friends'), (b'FRIENDS', b'Friends'), (b'PRIVATE', b'Only Me'), (b'SERVERONLY', b'Only Server')])),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(related_name='comments', to='api.Post'),
         ),
     ]
