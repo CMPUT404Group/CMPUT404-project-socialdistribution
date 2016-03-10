3ffd4b25fccdde0bb2d2b7d8ead7f398d3fc937f
jlx339 <jlx339@hotmail.com>
2016-03-09 18:47:51 -0700

    CrFixed  bugs in view.py

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from api.models import Post, Comment, Upload, Image
+from api.models import Post, Comment, Upload, Image, Following, Friending
 from api.serializers import PostSerializer, CommentSerializer, ImageSerializer
 from api.serializers import UserSerializer
 from rest_framework.decorators import api_view
@@ -216,7 +216,7 @@ Gets a specific Comment/ Updates a Comment / Deletes a Comment
 
 class CommentDetail(generics.GenericAPIView):
     serializer_class = CommentSerializer
-    queryset = Comment.objects.all()Post
+    queryset = Comment.objects.all()
 
     def isAllowed(self,request,pk):
         post = self.get_object(pk)
@@ -312,11 +312,6 @@ class FriendingList(generics.GenericAPIView):
     pagination_class = ListPaginator
     serializer_class = CommentSerializer
     queryset = Friending.objects.all()
-
-    def addfriend(){
-
-
-
 
 
 '''
