237d315c07c057b2d06e26e37248ba4ab0d2c7ad
genevag <geneva@ualberta.ca>
2016-03-10 14:24:58 -0700

    admin author & user + fix to user's being able to change other users profile pictures

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
                 ('comment', models.TextField()),
                 ('contentType', models.CharField(default=b'text/plain', max_length=15, choices=[(b'text/x-markdown', b'Markdown'), (b'text/plain', b'Plaintext')])),
                 ('published', models.DateTimeField(default=django.utils.timezone.now)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(to='api.Author')),
             ],
         ),
         migrations.CreateModel(
@@ -64,7 +64,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('photo', models.ImageField(upload_to=b'images/', verbose_name=b'Image')),
                 ('upload_date', models.DateTimeField(auto_now_add=True)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(to='api.Author')),
             ],
         ),
         migrations.CreateModel(
@@ -77,7 +77,7 @@ class Migration(migrations.Migration):
                 ('published', models.DateTimeField(default=django.utils.timezone.now)),
                 ('visibility', models.CharField(default=b'FRIENDS', max_length=18, choices=[(b'PUBLIC', b'Public'), (b'FOAF', b'Friends of Friends'), (b'FRIENDS', b'Friends'), (b'PRIVATE', b'Only Me'), (b'SERVERONLY', b'Only Server')])),
                 ('image_url', models.CharField(max_length=200, null=True, blank=True)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('author', models.ForeignKey(to='api.Author')),
             ],
         ),
         migrations.CreateModel(
