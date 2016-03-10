7ca6d7f41296e6f230bf1baf52e7e4ad12eeb9ef
cheng10 <cheng10@ualberta.ca>
2016-03-10 07:41:43 -0700

    customized admin site mostly done, now admin can easily toggle user active status

--- api/admin.py
+++ api/admin.py
@@ -12,14 +12,16 @@ class UserAdmin(admin.ModelAdmin):
         AuthorInline,
     ]
     fields = {'username', 'is_active', 'email'}
+    list_display = ('username', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+# admin.site.register(Author)
 # admin.site.register(Post)
 # admin.site.register(Comment)
 # admin.site.register(Upload)
-# admin.site.register(Author)
 # admin.site.register(Friending)
 # admin.site.register(Following)
 
