090df254ff50d8f36d7aad318fa082a65629ab7b
falon3 <scheers@ualberta.ca>
2016-03-11 23:49:01 -0700

    oops can't have friends as path

--- api/views.py
+++ api/views.py
@@ -27,7 +27,6 @@ def api_root(request, format=None):
         'images': reverse('images', request=request, format=format),
         'befriend': reverse('befriend', request=request, format=format),
         'friendrequest': reverse('friendrequest', request=request, format=format),
-        'friends': reverse('friends', request=request, format=format),
     })
 
 
