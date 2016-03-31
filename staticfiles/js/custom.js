16e63b1ecde2ad86210098b1e5bebf8388f4d429
genevag <geneva@ualberta.ca>
2016-03-30 18:34:59 -0600

    image fix local

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -164,7 +164,7 @@ window.onload = function() {
         // disable add image button in create post form
         $("#uploadImageTrigger").prop("disabled", true);
         // add image url to form's hidden image_url field (on create post form)
-        $("input#id_image_url").val(response.photo);
+        $("input#id_image").val(response.photo);
         toastr.info("Image Uploaded!");
       },
       error: function(xhr, ajaxOptions, error) {
